01a8c123d3187e704928b8f5292c6b94e03c9bb0
Carlos Pedreira <carlospedreira@outlook.com>
2024-03-14 14:34:37 +0000

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+using System.IO.Hashing;
+
+var data = new { Foo = "Bar" };
+
+var json = JsonSerializer.Serialize(data);
+var bytes = JsonSerializer.SerializeToUtf8Bytes(data);
+
+var hash = BitConverter.ToString(bytes).Replace("-", "").ToLower();
+
+Console.WriteLine($"{data} => {hash}");
